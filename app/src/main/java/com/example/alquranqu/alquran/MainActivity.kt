35ee8998acc341b722cf629e57a75b978b920979
suswantiaandriani <suswantiaandriani12@gmail.com>
2021-06-21 21:01:17 +0700

    Project 3

---
+++ app/src/main/java/com/example/alquranqu/alquran/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.alquranqu.alquran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.alquran.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
